f3268327d0d59c12cd27faf780cd60be88b22b11
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-28 14:54:08 +0530

    initial commit

---
+++ java_stdin_and_stdout-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int i=sc.nextInt();
+        sc.nextLine();
+        Double d=sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
