66fbc7aea10ded2202b9cb4988af4ed3205c0aba
Rishit Bhojak <rajabhojak@gmail.com>
2021-01-30 12:20:08 +0530

    initial commit

---
+++ java_end_of_file.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+int a=0;
+while(scan.hasNext()){
+System.out.printf("%d %s\n",++a,scan.nextLine());
+}
+}
+}
